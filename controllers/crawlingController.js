1b829637be7d2ddf5a3d343fbf8ed48575b61363
kimjames <canvasbook@inu.ac.kr>
2020-05-17 21:51:42 +0900

    uddate detail Cralwer, Fix clien crawler

--- controllers/crawlingController.js
+++ controllers/crawlingController.js
@@ -197,22 +197,19 @@ const coolEnjoy = async (pageNum, callback) => {
     const response = await axios({
       method: "GET",
       url: link,
-      // headers:
-      // {
-      //   "scheme": 'https',
-      //   "accept": 'application / json, text/ javascript */*; q=0.01',
-      //   'accept-encoding': 'gzip, deflate, br',
-      //   'accept-language': 'ko- KR, ko; q = 0.9, en - US; q = 0.8, en; q = 0.7',
-      //   "Connection": 'keep-alive',
-      //   "Cookie": 'SESSION=2aa1770f-d213-42d4-9993-280d008bca90; SCOUTER=x5evhpumo7oqih; _ga=GA1.2.860498838.1587006483; _gid=GA1.2.896276763.1589517326',
-      //   'Host': 'www.clien.net',
-      //   'referer': link,
-      //   'sec-fetch-dest': 'empty',
-      //   'sec-fetch-mode': 'cors',
-      //   'sec-fetch-site': 'same-origin',
-      //   'user-agent': ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
-      //   'X-Requested-With': 'XMLHttpRequest',
-      // }
+      headers:
+      {
+        "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-encoding': 'gzip, deflate',
+        'accept-language': 'ko- KR, ko; q = 0.9, en - US; q = 0.8, en; q = 0.7',
+        'Cache-Control': 'max-age=0',
+        "Connection": 'keep-alive',
+        "Cookie": '__utmz=192136120.1589469549.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); e1192aefb64683cc97abb83c71057733=amlydW0%3D; 2a0d2363701f23f8a75028924a3af643=MTc1LjEyNC4xODguMTI0; ck_font_resize_rmv_class=; ck_font_resize_add_class=; __utma=192136120.416924668.1589469549.1589564280.1589713933.3; __utmc=192136120; __utmt=1; __utmb=192136120.5.10.1589713933',
+        'Host': 'www.coolenjoy.net',
+        'Upgrade-Insecure-Requests': '1',
+        'Referer': 'http://www.coolenjoy.net/bbs/jirum',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+      }
 
     });
 
@@ -262,9 +259,10 @@ const clien = async (pageNum, callback) => {
         "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'accept-encoding': 'gzip, deflate, br',
         'accept-language': 'ko- KR, ko; q = 0.9, en - US; q = 0.8, en; q = 0.7',
+        'Cache-Control': 'max-age=0',
         "Connection": 'keep-alive',
-        "Cookie": 'SESSION=016e338b-1425-4912-afa0-4744e4737b67; SCOUTER=x5evhpumo7oqih; _ga=GA1.2.860498838.1587006483; _gid=GA1.2.896276763.1589517326',
         'Host': 'www.clien.net',
+        'Cookie': 'SESSION=2c79c83d-137e-4eee-b8be-ceb8bc82d25e; SCOUTER=x5evhpumo7oqih; _ga=GA1.2.860498838.1587006483; _gid=GA1.2.1622512015.1589695048; _gat_gtag_UA_682411_1=1',
         'referer': 'https://www.clien.net/service/board/jirum',
         'sec-fetch-dest': 'document',
         'sec-fetch-mode': 'navigate',
@@ -272,7 +270,7 @@ const clien = async (pageNum, callback) => {
         'sec-Fetch-User': '?1',
         'Upgrade-Insecure-Requests': '1',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
-      }
+      },
 
     });
 
@@ -474,6 +472,7 @@ const ppoumpuHotdeal = async (page, callback) => {
 exports.itemSpec = (keyword) => new Promise(async (resolve, reject) => {
   try {
     search = await danawaSearch(keyword)
+    inFo = await prodData(search.hidden)
     formdata = querystring.stringify(
       {
         pcode: search.hidden,
@@ -481,16 +480,16 @@ exports.itemSpec = (keyword) => new Promise(async (resolve, reject) => {
         cate2: search.cate[1],
         cate3: search.cate[2],
         cate4: search.cate[3],
-        makerName: '', // 미완성
-        brandName: '', // 미완성
-        makerUrl: '', // 미완성
-        kccode: '',  // 미완성
-        kpscode: '',  // 미완성
-        circulName: '',
-        circulUrl: '',
-        productName: '',
-        prodType: '',
-        displayMakeDate: search.date,
+        makerName: inFo.makerName, // 미완성
+        brandName: inFo.brandName, // 미완성
+        makerUrl: inFo.makerUrl, // 미완성
+        kccode: inFo.kccode,  // 미완성
+        kpscode: inFo.kpscode,  // 미완성
+        circulName: inFo.circulName,
+        circulUrl: inFo.circulUrl,
+        productName: inFo.productName,
+        prodType: inFo.prodType,
+        displayMakeDate: inFo.displayMakeDate,
         productFullName: search.title
       }
     )
@@ -522,17 +521,35 @@ exports.itemSpec = (keyword) => new Promise(async (resolve, reject) => {
     const html = response.data;
     let ulList = [];
     const $ = cheerio.load(html);
-    const $bodyList = $('[class$="spec_tbl"]')
+    const $bodyList = $('[class$="spec_tbl"]').find('tbody tr')
+    speclist = []
+    speclist1 = new Object()
+    speclist2 = new Array()
+    keyname = ''
 
     if (response.status == 200) {
       $bodyList.each(function (i, elem) {
-        ulList[i] = {
-          spec: $(this).find('tbody tr').text().replace(/(\n)/g, "/").replace(/(\t)/g, ""),
-        };
-      });
+        if ($(this).children().length == 4 || $(this).children().length == 2) {
+          // speclist.push($(this).find('th:nth-child(1)').text().replace(/\n/g, '') + ': ' + $(this).find('td:nth-child(2)').text().trim().replace(/\t/g, '').replace(/\n/g, ''))
+          // speclist.push($(this).find('th:nth-child(3)').text() + ':' + $(this).find('td:nth-child(4)').text())
+          // console.log(speclist)
+          // b = $(this).find('th:nth-child(1)').text()
 
-      const data = ulList;
-      resolve(data);
+          // speclist1[$(this).find('th:nth-child(3)').text()] = $(this).find('td:nth-child(4)').text().trim().replace(/\t/g, '')
+          if ($(this).children().length != 2) {
+            if ($(this).find('td:nth-child(2)').text().trim().replace(/\t/g, '') != '') {
+              speclist1[$(this).find('th:nth-child(1)').text()] = $(this).find('td:nth-child(2)').text().trim().replace(/\t/g, '').replace(/\n/g, '')
+            }
+            if ($(this).find('td:nth-child(4)').text().trim().replace(/\t/g, '') != '') {
+              speclist1[$(this).find('th:nth-child(3)').text()] = $(this).find('td:nth-child(4)').text().trim().replace(/\t/g, '')
+            }
+          }
+        }
+
+
+      });
+      await speclist2.push(speclist1)
+      resolve(speclist2);
     }
 
   } catch (err) {
@@ -574,7 +591,7 @@ const danawaSearch = (keyword) => new Promise(async (resolve, reject) => {
           'Cookie': 'danawa-loggingApplicationClient=0e2b04cf-e9aa-4460-881a-3136dbba27f5; _ga=GA1.2.208717042.1583803510; ADWEBCOUNTER_UUID=abd22176-5744-20e2-9b38-1daa02caab9a; ADWEBCOUNTER_KEYWORD=; OAX=r3y8fF6W9WwAAGIN; ADWEBCOUNTER_URL=google.com; _INSIGHT_CK_8204=2d19ec171f979094b13c13185ef31a6b_47312|11792e1c8fbe00a6335e13185ef31a6b_47313:1588149114000; DPG_MOBILE_ARTICLE_MOVE_GUIDE=false; cookSaveShopInfo=EE128%3A2020-05-08; recentProductYN=Y; cookSaveProdInfo=12%3A10931007%3A495000%7C11%3A5937666%3A33300%7C11%3A4678115%3A42000%7C11%3A7342828%3A169500%7C18%3A8733146%3A77430%7C12%3A10234701%3A866100%7C12%3A8946266%3A754940%7C11%3A6160126%3A92720%7C12%3A9492972%3A435000%7C17%3A10515417%3A25000%7C19%3A7419571%3A8900%7C11%3A8100778%3A32900%7C12%3A6470540%3A145000%7C16%3A10590585%3A40920%7C12%3A6468060%3A30000; _gid=GA1.2.1214192827.1589157765; cookieGuestId=e41434714c4597a6bd319affc921b38a; cookNewSearchKeyword=%EA%B0%A4%EB%9F%AD%EC%8B%9C%ED%83%ADs6%EB%9D%BC%EC%9D%B4%ED%8A%B8%3E05.11%7CDDR4-2666%3E05.04%7C%EB%B3%B5%ED%95%A9%EA%B8%B0%3E04.26%7C%EC%95%84%EC%9D%B4%ED%8C%A8%EB%93%9C2%20%EC%A4%91%EA%B3%A0%EA%B0%80%EA%B2%A9%3E04.24%7C%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90%20%EA%B0%A4%EB%9F%AD%EC%8B%9C%ED%83%ADS5e%2010.5%20LTE%20128G%3E04.20%7C%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90%20%EA%B0%A4%EB%9F%AD%EC%8B%9C%ED%83%ADS5e%2010.5%2064G%3E04.20%7C%ED%8C%85%EB%A7%A4%EC%B9%984%ED%98%B8%20%EB%9D%BC%EC%9D%B4%ED%8A%B8%20%EA%B2%BD%EB%9F%89%EC%B6%95%EA%B5%AC%EA%B3%B5%2011%EC%84%B8%EC%9D%B4%ED%95%98%20%EC%95%84%EB%8F%99%EC%9A%A9%EC%B6%95%EA%B5%AC%EA%B3%B5%3E04.20%7C%EC%8A%A4%ED%83%80%EC%8A%A4%ED%8F%AC%EC%B8%A0%20%EB%AA%AC%ED%83%80%EB%82%98%205%ED%98%B8%20%EC%B6%95%EA%B5%AC%EA%B3%B5%20SB895%3E04.20%7C%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90%20%EC%82%BC%EC%84%B1%20%EA%B0%A4%EB%9F%AD%EC%8B%9C%ED%83%ADS6%2010.5%20WiFi%20128G%3E04.20%7C%EA%B0%A4%EB%9F%AD%EC%8B%9C%ED%83%ADs6%3E04.20; cPreviousKeyword=%EA%B0%A4%EB%9F%AD%EC%8B%9C%ED%83%ADs6%EB%9D%BC%EC%9D%B4%ED%8A%B8; _INSIGHT_CK_8203=dbd08b20fb13b1d7f25fbd4e26cddbe1_03510|2646020e3363a1be76dcf97ad38a5d15_75222:1589177022000; wcs_bt=s_3b3fb74948b1:1589175222',
           'Host': 'search.danawa.com',
           'Origin': 'http://search.danawa.com',
-          'Referer': 'http://search.danawa.com/dsearch.php?&module=goods&act=dispMain',
+          'Referer': `http://search.danawa.com/dsearch.php?&module=goods&act=dispMain`,
           'UserAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
           'XRequestedWith': 'XMLHttpRequest',
         },
@@ -609,6 +626,53 @@ const danawaSearch = (keyword) => new Promise(async (resolve, reject) => {
     reject(err)
   }
 });
+
+const prodData = async (pcode, callback) => {
+  try {
+
+    link = "http://prod.danawa.com/info/?pcode=" + pcode
+    const response = await axios(
+      {
+        method: "GET",
+        url: link,
+        headers: {
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+          'AcceptEncoding': 'gzip, deflate',
+          'AcceptLanguage': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+          'Cache-Control': 'max-age=0',
+          'Connection': 'keep-alive',
+          'ContentLength': '316',
+          'ContentType': 'application/x-www-form-urlencoded',
+          'Cookie': 'ADWEBCOUNTER_UUID=03bd7cbf-77c5-209c-918c-f6fd954a64b0; danawa-loggingApplicationClient=0e2b04cf-e9aa-4460-881a-3136dbba27f5; _ga=GA1.2.208717042.1583803510; ADWEBCOUNTER_KEYWORD=; OAX=r3y8fF6W9WwAAGIN; _INSIGHT_CK_8204=2d19ec171f979094b13c13185ef31a6b_47312|11792e1c8fbe00a6335e13185ef31a6b_47313:1588149114000; recentProductYN=Y; ADWEBCOUNTER_URL=; _gid=GA1.2.1105565467.1589469673; cookSaveShopInfo=EE128%3A2020-05-08%7CTP402%3A2020-05-15; cookNewSearchKeyword=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90%20%EA%B0%A4%EB%9F%AD%EC%8B%9C%ED%83%ADS6%20%EB%9D%BC%EC%9D%B4%ED%8A%B8%3E05.16%7C%EA%B0%A4%EB%9F%AD%EC%8B%9C%ED%83%ADs6%EB%9D%BC%EC%9D%B4%ED%8A%B8%3E05.16%7Cqcy%20t5%20pro%3E05.16%7C%EB%8B%8C%ED%85%90%EB%8F%84%20%EC%8A%A4%EC%9C%84%EC%B9%98%3E05.16%7Cqcy%20t5%3E05.16%7C%EA%B0%A4%EB%9F%AD%EC%8B%9C%ED%83%ADS6%20%EB%9D%BC%EC%9D%B4%ED%8A%B8%2010.4%20LTE%2064GB%3E05.15%7Cusb%20%ED%97%88%EB%B8%8C%3E05.13%7C%EC%82%BC%EC%84%B1%20ddr4%208g%20pc4-21300%3E05.13%7C%EC%82%BC%EC%84%B1%3E05.13%7C%ED%83%9C%EB%B8%94%EB%A6%BF%3E05.13; cookieGuestId=7cce453fa2e1d529b39f199a3678ea44; PHPSESSID=ffed6uadb6hbudncph9luqc0a7; _gat=1; cookSaveProdInfo=12%3A10929630%3A439000%7C10%3A11206107%3A26650%7C19%3A8706542%3A555930%7C11%3A5937666%3A32920%7C10%3A10893660%3A19420%7C10%3A10906536%3A15070%7C10%3A9959532%3A26580%7C10%3A9921900%3A18980%7C11%3A8080807%3A31500%7C18%3A4666038%3A12370%7C15%3A10409847%3A247000%7C12%3A10931013%3A475000%7C11%3A3274953%3A11810%7C15%3A4423224%3A345000%7C12%3A10931007%3A474390; wcs_bt=s_3b3fb74948b1:1589583579; _INSIGHT_CK_8203=dbd08b20fb13b1d7f25fbd4e26cddbe1_03510|f88f992353f7e0cc73389055118e14f0_83009:1589585381000',
+          'Host': 'prod.danawa.com',
+          // 'Origin': 'http://search.danawa.com',
+          // 'Referer': 'http://search.danawa.com/dsearch.php?&module=goods&act=dispMain',
+          'UserAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+          // 'XRequestedWith': 'XMLHttpRequest',
+        },
+        // enconding: 'utf-8'
+      }
+    );
+
+
+    console.log(response.status)
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const scripts = $('script').filter(function () {
+      return ($(this).html().indexOf('var oProductDescriptionInfo =') > -1);
+    });
+    if (scripts.length === 1) {
+      const text = $(scripts[0]).html();
+      const oProductDescriptionInfo = text.split('var oProductDescriptionInfo = ')[1].split('};')[0] + '}'
+      const productInfo = JSON.parse(oProductDescriptionInfo)
+      console.dir(productInfo)
+      return productInfo
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 exports.itemDetail = (req, res) => {
   console.log('상품 상세 정보 조회 호출됨.');
--- controllers/youtubeController.js
+++ controllers/youtubeController.js
@@ -1,25 +1,25 @@
-var {google} = require('googleapis');
+var { google } = require('googleapis');
 var request = require('request');
 var querystring = require('querystring');
 var util = require('../middleware/util');
 const crawlingController = require('./crawlingController.js');
-const YOUTUBE_API_KEY = 'AIzaSyC7YY58-0d5LffCoYBHUlYZCqFKOJawxwQ';
+const YOUTUBE_API_KEY = 'AIzaSyCe41JVtYAPPC8a5yOQZJkwuKFLJMGHT7A';
 
 exports.search = (req, res) => {
 
-	var options = {
-		q: req.query.query,
-		part: "snippet",
-		key: YOUTUBE_API_KEY,
-		type: "video",
-		// maxResults: req.query.maxResults,
-		regionCode: "KR"
-	};
+    var options = {
+        q: req.query.query,
+        part: "snippet",
+        key: YOUTUBE_API_KEY,
+        type: "video",
+        // maxResults: req.query.maxResults,
+        regionCode: "KR"
+    };
 
-	var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search?' + querystring.stringify(options);
+    var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search?' + querystring.stringify(options);
 
-	request.get(youtubeUrl, function(err, response, body){
-		if(!err && response.statusCode == 200){
+    request.get(youtubeUrl, function (err, response, body) {
+        if (!err && response.statusCode == 200) {
             console.log('유튜브 API 검색 성공');
             let data = JSON.parse(body);
             res.status(response.statusCode);
@@ -37,25 +37,25 @@ exports.search = (req, res) => {
 
 exports.searchToTitle = (req) => new Promise((resolve, reject) => {
     var options = {
-		q: req.query.query,
-		part: "snippet",
-		key: YOUTUBE_API_KEY,
-		type: "video",
-		// maxResults: req.query.maxResults,
-		regionCode: "KR"
-	};
+        q: req.query.query,
+        part: "snippet",
+        key: YOUTUBE_API_KEY,
+        type: "video",
+        // maxResults: req.query.maxResults,
+        regionCode: "KR"
+    };
 
-	var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search?' + querystring.stringify(options);
+    var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search?' + querystring.stringify(options);
 
-	request.get(youtubeUrl, async function(err, response, body){
-		if(!err && response.statusCode == 200){
+    request.get(youtubeUrl, async function (err, response, body) {
+        if (!err && response.statusCode == 200) {
             let searchResult = [];
             let spec = await crawlingController.itemSpec(req.query.query);
             searchResult.push(spec[0]);
-            for(let i = 0;i < JSON.parse(body).items.length; i++){
+            for (let i = 0; i < JSON.parse(body).items.length; i++) {
                 searchResult.push(JSON.parse(body).items[i]);
             }
-            
+
             console.log('유튜브 API 검색 성공');
 
             resolve(searchResult);
